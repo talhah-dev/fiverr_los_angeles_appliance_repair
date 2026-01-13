b98240773eea184ec396264011be33d6e97f1b2b
M Talha <talha18513@gmail.com>
2026-01-13 21:03:11 +0500

    ui pahse

--- docs/javascript/global.js
+++ docs/javascript/global.js
@@ -2,28 +2,28 @@ const lenis = new Lenis({
   autoRaf: true,
 });
 
-// const bottomToTopScroll = document.getElementById("bottomToTopScroll");
+const bottomToTopScroll = document.getElementById("bottomToTopScroll");
 
-// bottomToTopScroll.innerHTML = `
-// <div
-//     class="bottomToTop fadeIn w-10 cursor-pointer z-40 bg-[#3da1b9] h-10 fixed bottom-5 right-5 hover:bg-[#192531] transition-all duration-500 hidden text-white flex items-center justify-center rounded-lg "><i class="fa-solid fa-angle-up"></i>
-// </div>`
+bottomToTopScroll.innerHTML = `
+<div
+    class="bottomToTop fadeIn w-10 cursor-pointer z-40 bg-[#1c398e] h-10 fixed bottom-5 right-5 hover:bg-[#1c398e]/80 transition-all duration-500 hidden text-white flex items-center justify-center rounded-lg "><i class="fa-solid fa-angle-up"></i>
+</div>`
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const scrollToTopBtn = document.querySelector(".bottomToTop");
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopBtn = document.querySelector(".bottomToTop");
 
-//   window.addEventListener("scroll", function () {
-//     if (window.scrollY > 200) {
-//       scrollToTopBtn.style.display = "flex";
-//     } else {
-//       scrollToTopBtn.style.display = "none";
-//     }
-//   });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      scrollToTopBtn.style.display = "flex";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
 
-//   scrollToTopBtn.addEventListener("click", function () {
-//     window.scrollTo({ top: 0, behavior: "smooth" });
-//   });
-// });
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
 
 AOS.init({
   once: true,
